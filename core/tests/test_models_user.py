7e50092ff4769c045a74c04c310dc5121386c391
douzez <douzez.courses@gmail.com>
2021-09-28 01:41:15 -0400

    add unit test for superuser

--- core/tests/test_models_user.py
+++ core/tests/test_models_user.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
-class ModelTests(TestCase):
+class ModelTestUser(TestCase):
     def test_create_user_with_email_successful(self):
         """ Test creating a new user with an email is successful """
         email = 'test@example.com'
@@ -22,3 +22,24 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, password)
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Test creating user with no email raises error """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1234')
+
+
+class ModelTestSuperUser(TestCase):
+    def test_create_new_superuser(self):
+        """ Test creating new super user """
+        user = get_user_model().objects.create_superuser(
+            'super@user.com', 'superpass123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_invalid_email_no_create_superuser(self):
+        """ Test does not create superuser with invalid email """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_superuser(None, 'qwertyu')
