a279a278504b66b60de3683ec54c48852127aeb7
douzez <douzez.courses@gmail.com>
2021-10-14 20:55:50 -0400

    add tests for token route

--- users/tests/test_token_public_api.py
+++ users/tests/test_token_public_api.py
@@ -33,3 +33,29 @@ class PublicTokenApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertIn('token', res.data)
         # rest.data = {'token': 'c09cfdf54096c3d5a8e8bb25415ccddaf0051f30'}
+
+    def test_dont_create_token_with_invalid_credentials(self):
+        """ Test that token is not created with invalid credentials """
+        create_user(email='email@test.com', password='wert1234', name='Test')
+
+        res = self.client.post(TOKEN_URL, self.payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        # res.data => {'non_field_errors': [ErrorDetail(string='Unable to authenticate with provided credentials', code='authentication')]}
+
+    def test_dont_create_token_with_no_user(self):
+        """ Test that token is not created if user doesn't exist """
+        res = self.client.post(TOKEN_URL, self.payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        # res.data => {'non_field_errors': [ErrorDetail(string='Unable to authenticate with provided credentials', code='authentication')]}
+
+    def test_dont_create_token_with_missing_field(self):
+        """ Test that token in not created with email and password are required """
+        res = self.client.post(TOKEN_URL, {'email': '', 'password': ''})
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        # res.data => {'email': [ErrorDetail(string='This field may not be blank.', code='blank')], 'password': [ErrorDetail(string='This field may not be blank.', code='blank')]}
